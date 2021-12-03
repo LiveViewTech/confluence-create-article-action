8f186b7d3349b0663ad05cef5c857ead5ddb705f
Connor Widtfeldt <connor.widtfeldt@liveviewtech.com>
2021-12-03 16:33:58 -0700

    Add strict option

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ if not workspace:
     raise Exception('No workspace is set')
 
 envs: Dict[str, str] = {}
-for key in ['parent', 'space', 'title', 'cloud', 'user', 'token']:
+for key in ['parent', 'space', 'title', 'cloud', 'user', 'token', 'strict']:
     value = environ.get(f'INPUT_{key.upper()}')
     if not value:
         raise Exception(f'Missing value for {key}')
@@ -33,11 +33,16 @@ content = {
     }
 }
 
-created = requests.post(url, json=content, auth=(envs['user'], envs['token'])).json()
-id = created['id']
-link = created['_links']['base'] + created['_links']['webui']
+try:
+    created = requests.post(url, json=content, auth=(envs['user'], envs['token'])).json()
+    id = created['id']
+    link = created['_links']['base'] + created['_links']['webui']
 
-print(f'::set-output name=id::{id}')
-print(f'::set-output name=url::{link}')
+    print(f'::set-output name=id::{id}')
+    print(f'::set-output name=url::{link}')
 
-print(f'Created new page: {link}')
+    print(f'Created new page: {link}')
+except:
+    print("Task failed")
+    if envs['strict'] == "true":
+        raise Exception("A problem occurred when creating the article")
